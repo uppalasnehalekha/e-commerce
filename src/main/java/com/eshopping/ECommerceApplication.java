eb0a2d5f431d1d9edf71751afe3dcc99b8172bf2
uppalasnehalekha <u.snehalekha@iiitdmj.ac.in>
2019-05-28 18:48:30 +0530

    swagger changes

--- src/main/java/com/eshopping/ECommerceApplication.java
+++ src/main/java/com/eshopping/ECommerceApplication.java
@@ -20,7 +20,7 @@ public class ECommerceApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.eshopping")).build();
 	}
 
 }
